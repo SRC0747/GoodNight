35ca97b16f3aee7350d40e74c5f5b990d872901c
SRC0747 <srcsrk1998@gmail.com>
2021-08-04 22:23:19 +0530

    Initial commit

---
+++ src/GoodNight.java
@@ -0,0 +1,8 @@
+public class GoodNight {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi, How are you?");
+        System.out.println("Good Evening");
+    }
+}
---
+++ src/javaPractice/Main.java
@@ -0,0 +1,10 @@
+package javaPractice;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hi, How are you?");
+        System.out.println("Good Evening");
+    }
+}
